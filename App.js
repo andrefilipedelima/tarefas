cb30a67f567d1f9a7592b98745043a4c8391218d
André Filipe Teixeira de Lima <andrefilipedelima@hotmail.com>
2024-02-17 16:23:46 -0300

    Aprendendo React-Native

--- App.js
+++ App.js
@@ -1,20 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  FlatList
+ } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import Tarefa from './src/Tarefa';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  const [ tarefa, setTarefa ] = useState('');
+
+  const [ list, setList ] = useState([]);
+
+  function handleAdd() {
+    
+    if (tarefa === '') {
+      return;
+    }
+
+    const dados = {
+      key: Date.now(),
+      item: tarefa
+    }
+
+    setList(oldArray => [dados, ...oldArray]);
+
+    setTarefa('');
+  }
+
+  function handleDelete(item) {
+    let filtroItem = list.filter((tarefa) => {
+      return (tarefa.item !== item);
+    });
+
+    setList(filtroItem);
+  }
+
+
+  return(
+    <View style={ styles.container }>
+      <Text style={ styles.title }>Tarefas</Text>
+
+      <View style={ styles.containerInput }>
+        <TextInput 
+          placeholder='Digite tua tarefa...'
+          style={ styles.input }
+          value={tarefa}
+          onChangeText={ (text) => setTarefa(text) }
+        />
+        <TouchableOpacity onPress={ handleAdd }
+          style={ styles.buttonAdd }
+        >
+          <FontAwesome name="plus" size={ 25 } color="#FFF" />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList 
+        data={ list }
+        keyExtractor={ (item) => item.key }
+        renderItem={ ({ item }) => <Tarefa data={ item } deleteItem={ () => handleDelete(item.item)}/> }
+        style={ styles.list }
+
+      />
     </View>
-  );
+  )
 }
+//flex 1 = tela inteira
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex:1,
+    backgroundColor: '#22272E',
+    paddinTop: 28
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#FFf',
+    marginTop: '5%',
+    paddingStart: '5%',
+    marginBottom: 12
+  },
+  containerInput: {
+    flexDirection: 'row',
+    width: '100%',
+    height: 44,
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 22
   },
-});
+  input: {
+    width: '75%',
+    backgroundColor: '#FBFBFB',
+    height: 44,
+    borderRadius: 4,
+    paddingHorizontal: 8
+  },
+  buttonAdd: {
+    width: '15%',
+    height: 44,
+    backgroundColor: '#73F7FF',
+    marginLeft: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 4
+  },
+  list: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    paddingStart: '4%',
+    paddingEnd: '4%'
+  }
+})
